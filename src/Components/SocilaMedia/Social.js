9512afa9d3fbbba53bf6cd656d4812c67e7eba92
malabdullah89 <53144490+malabdullah89@users.noreply.github.com>
2021-06-02 01:38:03 +0300

    social icons

--- src/Components/SocilaMedia/Social.js
+++ src/Components/SocilaMedia/Social.js
@@ -7,28 +7,28 @@ import twitter from './377674.png'
 const Social = () => {
     return (
         <div className="social">
-            <h4 className="title-social">Social Accounts</h4>
+            
            <div>
                 <h2><a href="https://github.com/malabdullah89"><img className="github" src={github} /></a>
-                <p className="git-p"><a className="social-link" href="https://github.com/malabdullah89">GitHub</a></p>
+                <p className="git-p"><a className="social-link" href="https://github.com/malabdullah89"></a></p>
                 </h2>
            </div>
 
            <div>
                 <h2><a href="https://www.youtube.com/channel/UCzHIbPbXhTAAGJldx1iYUGw"><img className="youtube" src={youtubeLogo} /></a>
-                <p className="git-p"><a className="social-link" href="https://www.youtube.com/channel/UCzHIbPbXhTAAGJldx1iYUGw">Youtube</a></p>
+                <p className="git-p"><a className="social-link" href="https://www.youtube.com/channel/UCzHIbPbXhTAAGJldx1iYUGw"></a></p>
                 </h2>
            </div>
 
            <div>
                 <h2><a href="https://www.instagram.com/mohmdev/</h2>"><img className="instagram" src={instagram} /></a>
-                <p className="git-p"><a className="social-link" href="https://www.instagram.com/mohmdev/">Instagram</a></p>
+                <p className="git-p"><a className="social-link" href="https://www.instagram.com/mohmdev/"></a></p>
                 </h2>
            </div>
 
            <div>
                 <h2><a href="https://twitter.com/alabdullahm"><img className="twitter" src={twitter} /></a>
-                <p className="git-p"><a className="social-link" href="https://twitter.com/alabdullahm">Twitter</a></p>
+                <p className="git-p"><a className="social-link" href="https://twitter.com/alabdullahm"></a></p>
                 </h2>
            </div>
            
