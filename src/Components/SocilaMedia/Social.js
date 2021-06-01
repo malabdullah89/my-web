c0119d829aad70e10ea746346c5560c4405e2092
malabdullah89 <53144490+malabdullah89@users.noreply.github.com>
2021-06-01 20:37:04 +0300

    add some animation

--- src/Components/SocilaMedia/Social.js
+++ src/Components/SocilaMedia/Social.js
@@ -1,29 +1,33 @@
 import './Social.css'
+import youtubeLogo from './youtube-logo.png'
+import github from './370919.png'
+import instagram from './instagram.png'
+import twitter from './377674.png'
 
 const Social = () => {
     return (
         <div className="social">
             <h4 className="title-social">Social Accounts</h4>
            <div>
-                <h2><a href="https://github.com/malabdullah89"><img className="github" src="https://icon-library.com/images/github-icon-white/github-icon-white-6.jpg" /></a>
+                <h2><a href="https://github.com/malabdullah89"><img className="github" src={github} /></a>
                 <p className="git-p"><a className="social-link" href="https://github.com/malabdullah89">GitHub</a></p>
                 </h2>
            </div>
 
            <div>
-                <h2><a href="https://www.youtube.com/channel/UCzHIbPbXhTAAGJldx1iYUGw"><img className="youtube" src="https://iconape.com/wp-content/files/cm/286303/svg/youtube-icon-logo-logo-icon-png-svg.png" /></a>
+                <h2><a href="https://www.youtube.com/channel/UCzHIbPbXhTAAGJldx1iYUGw"><img className="youtube" src={youtubeLogo} /></a>
                 <p className="git-p"><a className="social-link" href="https://www.youtube.com/channel/UCzHIbPbXhTAAGJldx1iYUGw">Youtube</a></p>
                 </h2>
            </div>
 
            <div>
-                <h2><a href="https://www.instagram.com/mohmdev/</h2>"><img className="instagram" src="http://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c521.png" /></a>
+                <h2><a href="https://www.instagram.com/mohmdev/</h2>"><img className="instagram" src={instagram} /></a>
                 <p className="git-p"><a className="social-link" href="https://www.instagram.com/mohmdev/">Instagram</a></p>
                 </h2>
            </div>
 
            <div>
-                <h2><a href="https://twitter.com/alabdullahm"><img className="instagram" src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png" /></a>
+                <h2><a href="https://twitter.com/alabdullahm"><img className="twitter" src={twitter} /></a>
                 <p className="git-p"><a className="social-link" href="https://twitter.com/alabdullahm">Twitter</a></p>
                 </h2>
            </div>
