5393e7d6fc5687db5c4d90180e94152e491829b1
malabdullah89 <53144490+malabdullah89@users.noreply.github.com>
2021-06-07 17:34:28 +0300

    ADD: Shahid project

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Projects.css'
 import DisneyImg from './viewers-disney.png'
+import ShahidLogo from './shahid-logo.svg'
  
 function Projects() {
     return (
@@ -8,7 +9,12 @@ function Projects() {
             <h4>Projects</h4>
             <div className="project-card">
                 <a href="https://disneymohmdev.herokuapp.com/"><img className="disney-logo" src={DisneyImg} /></a>
+                
+            </div>
 
+            <div className="shahid-card">
+                <a href="http://shahidnet.herokuapp.com/"><img className="disney-logo" src={ShahidLogo} /></a>
+                
             </div>
            
         </div>
